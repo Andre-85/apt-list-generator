dd2d2f3aa227cbe208bd14861b6a9c3ce13d1319
Andre Wagner <andre.wagner@richard-wolf.com>
2024-03-20 21:35:26 +0100

    Renaming

--- apt_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-import distro_info
-
-suffixes = ["", "security", "updates"]
-suffixes_extra = ["backports", "proposed" ]
-
-for d in distro_info.UbuntuDistroInfo().all:
-    print(d)
